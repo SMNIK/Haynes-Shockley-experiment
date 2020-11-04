5110675426593383e24a0aa4390d3a8091f96fd4
SMNIK <masood.milestone@studio.unibo.it>
2020-11-03 16:23:01 -0800

    change name

---
+++ testing.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 14:57:54 2020
+
+@author: masou
+"""
+# if we want to use the base data as CSV
+
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+# Import csv file and save as the new table
+
