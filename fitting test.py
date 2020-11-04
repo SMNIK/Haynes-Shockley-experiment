5898eeec4aa33f67f5836de1cb3d421f15fedcbe
SMNIK <masood.milestone@studio.unibo.it>
2020-11-03 17:50:57 -0800

    Create fitting test.py

---
+++ fitting test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 17:46:21 2020
+
+@author: masou
+"""
+
+import pandas as pd
+import matplotlib as plt
+from scipy import asarray as ar, exp
+import xlrd
+import tkinter as tk
+from tkinter import filedialog
+
+""" In this file we can import any excel file by the key and at the end 
+creat a fit function for each figure
+"""
+
