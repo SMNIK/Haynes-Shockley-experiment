d3b569683dd853cc4aae6eb72d250b72ee0a6dd2
SMNIK <masood.milestone@studio.unibo.it>
2020-11-03 18:04:53 -0800

    Update fitting test.py

--- fitting test.py
+++ fitting test.py
@@ -12,7 +12,39 @@ import xlrd
 import tkinter as tk
 from tkinter import filedialog
 
-""" In this file we can import any excel file by the key and at the end 
-creat a fit function for each figure
+""" 
+In this file, we can import an Excel file by the key, and at the end
+Create a fit function for each figure.
 """
 
+root = tk.Tk()
+canvas1 = tk.canvas(root, width=200, height=200, bg="lightgrey")
+canvas1.pack()
+
+"""
+we continue to code as the 'Button' file until the browser read all
+excel sheets and plot them.
+"""
+# command def to clear the file by the key
+def getExcel():
+    global SMN
+    import_file_path = filedialog.askopenfilename()
+    xls = xlrd.open_workbook(import_file_path, on_demand=True)
+    sheetNames = xls.sheet_names()
+    for i in sheetNames:
+        SMN = pd.read_excel(import_file_path, i)
+        SMN = SMN.iloc[400:3700]
+        print(i) # if put SMN, the console shows all datas of each sheet, but I shows the name of sheets, after the last name if you close the key, so plot is ready
+        #plt.plot(SMN['x'], SMN['y'])
+        plt.xlabel('Time (\u03BC s) \n Set of pulses collected at constant d=0.35cm, by varying the sweeping voltage $V_{s}$')
+        plt.ylabel('Voltage (v)')
+        plt.title('fit-black curve (Gaussian)')
+        plt.legend(sheetNames, fontsize=10, loc='upper right')
+    
+"""
+Now the top def and loop read each sheet and plot, so now we just need
+to call the data of each sheet as x and y column to create fit.
+the match a fit function and its coefficients.
+"""
+x = SMN['x']
+y = SMN['y']
