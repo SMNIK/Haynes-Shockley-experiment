deb1d415a9d602930bdc480beff6d5ff0b4e445d
SMNIK <masood.milestone@studio.unibo.it>
2020-11-03 16:14:25 -0800

    Update Practice File.py

--- Practice File.py
+++ Practice File.py
@@ -4,7 +4,9 @@ Created on Mon Nov  2 14:57:54 2020
 
 @author: masou
 """
+# if we want to use the base data as CSV
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
